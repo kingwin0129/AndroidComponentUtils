985a045d1463556ca59b9526f228867ac0e9bb07
KingWin <kingwin0129@163.com>
2021-05-25 15:28:57 +0800

    1.兼容Fragment沉浸式

--- immersion/src/main/java/com/kingwin/immersion/KImmersionUtils.java
+++ immersion/src/main/java/com/kingwin/immersion/KImmersionUtils.java
@@ -3,6 +3,7 @@ package com.kingwin.immersion;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
@@ -10,6 +11,7 @@ import android.widget.RelativeLayout;
 
 import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.kingwin.immersion.statusBar.KStatusBarUtils;
 import com.kingwin.immersion.titleBar.KTitleBar;
@@ -29,8 +31,11 @@ public class KImmersionUtils {
      * @param layputId 布局id
      * @param config 配置文件
      */
-    public static void init(AppCompatActivity activity, @LayoutRes int layputId, KImmersionConfig config){
-        activity.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+    public static void init(Activity activity, @LayoutRes int layputId, KImmersionConfig config){
+        if(activity instanceof AppCompatActivity){
+            ((AppCompatActivity)activity).supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        }
+
 
         RelativeLayout baseLayout = new RelativeLayout(activity);
 
@@ -57,4 +62,36 @@ public class KImmersionUtils {
         KStatusBarUtils.immersionBar(activity.getWindow());
     }
 
+
+    /**
+     * 初始化fragment布局
+     * @param fragment 所属碎片
+     * @param layputId 布局id
+     * @param config 配置文件
+     */
+    public static View inflate(Fragment fragment, LayoutInflater inflater, ViewGroup container, @LayoutRes int layputId, KImmersionConfig config){
+
+
+        RelativeLayout baseLayout = new RelativeLayout(fragment.getActivity());
+
+        KTitleBar titleBar = new KTitleBar(fragment.getActivity(),config);
+        titleBar.setId(R.id.ktitlebar);
+
+        ViewGroup inflate = (ViewGroup) inflater.inflate(layputId,container,false);
+
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        if(config.getMainLayoutRule() != -1){
+            lp.addRule(config.getMainLayoutRule(),titleBar.getId());
+        }
+
+        baseLayout.addView(inflate,lp);
+
+
+
+        baseLayout.addView(titleBar, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+
+        return baseLayout;
+    }
 }
